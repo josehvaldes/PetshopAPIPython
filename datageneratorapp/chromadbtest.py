88f02f2250a56f7b07b375bf2996b6ad14f17bbd
josehvaldes <jmurguia@gmail.com>
2025-09-07 22:10:23 -0400

    Use dog breeds docs in chromadbtest script

--- datageneratorapp/chromadbtest.py
+++ datageneratorapp/chromadbtest.py
@@ -1,22 +1,68 @@
-﻿import chromadb
-client = chromadb.Client() # Can be set to save to disk
-collection = client.create_collection("sales_reports")
+﻿import json
+import chromadb
 
-collection.upsert(
-    # documents=["Sales for week 1 were $500...", "Week 2 sales were $650..."],
-    # metadatas=[{"week": 1}, {"week": 2}],
-    # ids=["week1", "week2"]
-    documents=[
-        "This is a document about pineapple",
-        "This is a document about oranges"
-    ],
-    ids=["id1", "id2"]
-)
+jsonl_path = "c:/personal/_gemma/customdocs/converted/dogs_dataset.jsonl"
 
-results = collection.query(
-    #query_texts=["What were the weekly sales?"],
-    query_texts=["This is a query document about florida"], # Chroma will embed this for you
-    n_results=2 # how many results to return
-)
-print("Results:")
-print(results)
+def load_jsonl(path):
+    try:
+        with open(jsonl_path, 'r', encoding='utf-8') as file:
+            data = [json.loads(line) for line in file]
+        return data
+    except FileNotFoundError:
+        print(f"File not found: {jsonl_path}")
+        data = []
+    except Exception as e:
+        print(f"Error loading JSONL file: {e}")
+        data = []
+
+def extract_documents_and_ids(data):
+    breed_documents = []
+    breed_ids = []
+    metadatas = []
+    for record in data: 
+        input_text = record.get("input", "")
+        breed_documents.append(input_text);
+        index = input_text.find(" Nacionalidad: ") # find first space
+        if index != -1:
+            id = input_text[:index] # take first word as id
+        else:
+            id = input_text
+        breed_ids.append(id)
+
+        index_nationality = input_text.find(". Origen") # find first space
+        nationality = ""
+        if index_nationality != -1:
+            nationality = input_text[ index + len(" Nacionalidad: ") : index_nationality].strip() # take text between "Nacionalidad" and ". Origen"
+            metadatas.append({"source": "dogs_dataset.jsonl", "nacionalidad": nationality})
+        else:
+            metadatas.append({"source": "dogs_dataset.jsonl", "nacionalidad": "unknown: " + input_text[:index]})
+    return breed_documents, breed_ids, metadatas
+
+
+def main():
+    data = load_jsonl(jsonl_path)
+    print(f"Loaded {len(data)} records from {jsonl_path}")
+
+    breed_documents, breed_ids, metadatas = extract_documents_and_ids(data)
+    print("Metadatas:")
+    print(metadatas[:])
+    
+    client = chromadb.Client() # Can be set to save to disk
+    collection = client.create_collection("sales_reports")
+
+    collection.upsert(
+        documents=breed_documents,
+        ids=breed_ids,
+        metadatas=metadatas
+    )
+
+    results = collection.query(
+        query_texts=["Nacionalidad China"],
+        n_results=5, # how many results to return
+    )
+    print("Results:")
+    print(results["ids"])
+
+
+if __name__ == "__main__":
+    main()
