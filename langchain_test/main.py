36bca0087b5c9ee6ebf0a2b1fd28b0856c09d4df
josehvaldes <jmurguia@gmail.com>
2025-09-15 13:19:21 -0400

    Add langchain test scripts

---
+++ langchain_test/main.py
@@ -0,0 +1,29 @@
+﻿# %%
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from langchain.llms import HuggingFacePipeline
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from transformers import pipeline
+
+model_path = "h:/ML_Models/_gemma/model/gemma-2b-it"  # local folder
+
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+model = AutoModelForCausalLM.from_pretrained(model_path)
+print("Model loaded.")
+
+
+# %%
+pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=512)
+llm = HuggingFacePipeline(pipeline=pipe)
+print("Pipeline loaded.")
+
+
+prompt = PromptTemplate.from_template("Answer like a helpful agent: {question}")
+chain = LLMChain(llm=llm, prompt=prompt)
+
+response = chain.run("which dogs are good for apartments?")
+print(response)
+print("Response generated.")
+
+
